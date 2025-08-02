e3825e3f85664181754b7a712a4dc263efa9b136
Gamnraa <ltaz1466@gmail.com>
2025-08-01 23:28:17 -0400

    Win state

--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 
 func _on_lose_body_entered(body: Node2D) -> void:
-	print(body, Global.ThePlayer)
 	if body.name == "Player":
 		Global.reset_player(false)
+
+
+func _on_win_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		Global.reset_player(true)
